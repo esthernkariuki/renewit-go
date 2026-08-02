6cf28a3929140e42cf5958e44fcee7059cdc420e
Esther Nyambura <esthernnyamburaa@gmail.com>
2026-08-02 19:56:35 +0300

    created gitignore

--- internal/catalogue/model.go
+++ internal/catalogue/model.go
@@ -6,9 +6,9 @@ import (
 
 type Catalogue struct {
 	ID                  uint      `json:"id" gorm:"primaryKey"`
-	MaterialType        string    `json:"material_type"`
-	PricePerKg          float64   `json:"price_per_kg"`
+	MaterialType        string    `json:"material_type" binding:"required"`
+	PricePerKg          float64   `json:"price_per_kg" binding:"required"`
 	LastUpdateDate      time.Time `json:"last_update_date"`
-	MaterialDescription string    `json:"material_description"`
+	MaterialDescription string    `json:"material_description" binding:"required"`
 	CreatedAt           time.Time `json:"created_at"`
 }
